505f72cbf3f2a92e63808e9071e47467922ece49
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2022-02-22 00:17:36 +0100

    renamed item_name to name in models.py file and index.html

--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Inventory(models.Model):
     image = models.CharField(max_length=250)
-    item_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     amount = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
